06c80a4ef72b7e2149046d12339f975b9cf2315c
Enrique <octepc.97@gmail.com>
2022-07-25 20:38:10 -0500

    watched how booleans work in TypeScript

--- src/04-numbers.ts
+++ src/04-numbers.ts
@@ -7,12 +7,6 @@
   customerAge = customerAge + 1
   console.log(customerAge)
 
-  let productInStock: number;
-  console.log("productInStock", productInStock);
-  if(productInStock>10){
-    console.log('is greater')
-  }
-
   let discount = parseInt('123');
   console.log('discount', discount)
   if(discount <=200){
