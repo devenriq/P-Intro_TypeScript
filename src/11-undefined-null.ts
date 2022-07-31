4a8677c7902bc0e70dddf78d605728fa21ec4587
Enrique <octepc.97@gmail.com>
2022-07-31 12:34:21 -0500

    finished the review of undefined and null types in TypeScript

--- src/11-undefined-null.ts
+++ src/11-undefined-null.ts
@@ -6,4 +6,30 @@
   let myNull:null = null;
   let myUndefined = undefined;
 
+  let myString: string | undefined = undefined;
+  myString= "hola"
+
+  function hi(name:string|null) {
+    let hello = 'hola,'
+
+    if (hello) {
+      hello+='name'
+    } else {
+      hello += 'nobody'
+    }
+    console.log(hello)
+  }
+
+  function hi2(name: string | null){
+    let hello = 'hola'
+    hello += name?.toLocaleLowerCase()||'nobody';
+    console.log(hello)
+  }
+
+  hi('Enrique')
+  hi(null)
+
+  hi('Octavio')
+  hi(null)
+
 })()
